21ca1c026ad017ab21c34b536dc0502f3c7162c8
stanfomini <stanford.matthew@outlook.com>
2020-02-06 12:31:47 -0700

    adding facebook mess 2

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
+import VueFbCustomerChat from 'vue-fb-customer-chat';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap';
-import VueFbCustomerChat from 'vue-fb-customer-chat'
 
 Vue.config.productionTip = false;
 
 Vue.use(VueFbCustomerChat, {
-	  page_id: 108398407383203, //  change 'null' to your Facebook Page ID,
-	  theme_color: '#333333', // theme color in HEX
-	  locale: 'en_US', // default 'en_US'
-})
+  page_id: 108398407383203,
+  theme_color: '#333333',
+  locale: 'en_US',
+});
 new Vue({
   router,
   render: h => h(App),
